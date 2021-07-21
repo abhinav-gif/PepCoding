e0a2276c8d8cc298810819b82c7f463ea7682655
Abhinav <17122000abhinav@gmail.com>
2021-07-21 18:16:25 +0530

    commit

--- AnyBaseAddition.cpp
+++ AnyBaseAddition.cpp
@@ -5,7 +5,7 @@ int add(int n, int m, int base)
 {
     int carry = 0;
     int ans = 0, y = 1;
-    while (n > 0 && m > 0)
+    while (n > 0 || m > 0||carry>0)
     {
         int ldn = n % 10;
         int ldm = m % 10;
@@ -17,29 +17,11 @@ int add(int n, int m, int base)
         n = n / 10;
         m = m / 10;
     }
-    while (n > 0)
-    {
-        int ldn = n % 10;
-        int sum = carry + ldn;
-        carry = sum / base;
-        sum = sum % base;
-        ans = ans + sum * y;
-        y = y * 10;
-        n = n / 10;
-    }while (m > 0)
-    {
-        int ldm = m % 10;
-        int sum = carry + ldm;
-        carry = sum / base;
-        sum = sum % base;
-        ans = ans + sum * y;
-        y = y * 10;
-        m = m / 10;
-    }
-    if (carry != 0)
-    {
-        ans = ans + carry * y;
-    }
+    
+    // if (carry != 0)
+    // {
+    //     ans = ans + carry * y;
+    // }
     return ans;
 }
 
