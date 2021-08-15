e9c27793e281f0049e4148eb287463a13ba9ac37
Abhinav <17122000abhinav@gmail.com>
2021-08-15 10:27:25 +0530

    commit

---
+++ LinkedList/LinkedList_Stack_Adapter.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+    int size;
+    LinkedList()
+    {
+        size = 0;
+        head = NULL;
+        tail = NULL;
+    }
+    void push(int data)
+    {
+        Node *n = new Node(data);
+        if (size == 0)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = tail->next;
+        }
+        size++;
+    }
+    int top()
+    {
+        if (size == 0)
+        {
+            cout << "Empty" << endl;
+            return -1;
+        }
+        else
+        {
+            return tail->data;
+        }
+    }
+    void pop()
+    {
+        if (size == 0)
+        {
+            cout << "Empty" << endl;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != tail)
+        {
+            temp = temp->next;
+        }
+        delete tail;
+        temp->next = NULL;
+        tail = temp;
+        size--;
+    }
+    int getSize()
+    {
+        return size;
+    }
+};
+
+int main()
+{
+    LinkedList l;
+    l.push(10);
+    l.push(20);
+    l.push(30);
+    cout << l.top() << " " << l.getSize() << endl;
+    l.pop();
+    cout << l.top() << " " << l.getSize() << endl;
+}
