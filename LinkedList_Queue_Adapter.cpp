907958811e46c10aadd861be09feb7e0cafb9e4f
Abhinav <17122000abhinav@gmail.com>
2021-08-15 22:26:03 +0530

    commit

---
+++ LinkedList_Queue_Adapter.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+class LinkedList
+{
+public:
+    Node *head;
+    Node *tail;
+    int size;
+    LinkedList()
+    {
+        size = 0;
+        head = NULL;
+        tail = NULL;
+    }
+    int getSize()
+    {
+        return size;
+    }
+    void add(int data)
+    {
+        Node *n = new Node(data);
+        if (size == 0)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = tail->next;
+        }
+        size++;
+    }
+    void remove()
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        size--;
+    }
+    int peek()
+    {
+        if (size == 0)
+        {
+            cout << "Empty" << endl;
+            return -1;
+        }
+        return head->data;
+    }
+};
+
+int main()
+{
+    LinkedList l;
+    l.add(10);
+    l.add(20);
+    l.add(30);
+    cout << l.peek() << endl;
+    l.remove();
+    cout << l.peek() << endl;
+}
