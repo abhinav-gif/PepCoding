f4c94b5fcf1ac26e376ef0f555b3a25799ab033c
Abhinav <17122000abhinav@gmail.com>
2021-08-27 21:56:21 +0530

    commit

---
+++ Stack/DupBracket.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isDup(string s)
+{
+    stack<char> s1;
+    for (int i = 0; i < s[i]; i++)
+    {
+        if (s[i] != ')' && s[i] != '}' && s[i] != ']')
+        {
+            s1.push(s[i]);
+        }
+        else
+        {
+            int count = 0;
+            while (s1.top() != '(' && s1.top() != '{' && s1.top() != '[')
+            {
+                s1.pop();
+                count++;
+            }
+            s1.pop();
+            if (count == 0)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    if (isDup(s))
+    {
+        cout << "True" << endl;
+    }
+    else
+    {
+        cout << "False" << endl;
+    }
+}
