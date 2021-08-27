1e05292dfd3aa87d481ae89a8d0e0543144ba25f
Abhinav <17122000abhinav@gmail.com>
2021-08-27 21:56:51 +0530

    commit

--- Stack/DupBracket.cpp
+++ Stack/DupBracket.cpp
@@ -5,7 +5,7 @@ using namespace std;
 bool isDup(string s)
 {
     stack<char> s1;
-    for (int i = 0; i < s[i]; i++)
+    for (int i = 0; i < s.length(); i++)
     {
         if (s[i] != ')' && s[i] != '}' && s[i] != ']')
         {
